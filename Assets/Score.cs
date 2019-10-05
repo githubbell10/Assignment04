188c3fa8367a0ee2877a143a73c8dcba2eaa3740
githubbell10 <52224530+githubbell10@users.noreply.github.com>
2019-10-05 15:16:43 -0500

    Initial commit

---
+++ Assets/Score.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour
+{
+    public static int PinCount = 0;
+    public Text text;
+    public void Start()
+    {
+        PinCount = 0;
+    }
+
+    public void Update()
+    {
+        text.text = PinCount.ToString();
+    }
+}
